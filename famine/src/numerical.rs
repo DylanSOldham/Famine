a93a30a3541015c58a66a6df5ac8e14f88e498e3
Dylan Oldham <dylansoldham@gmail.com>
2024-07-30 17:07:12 -0700

    Upgraded Euler solver to accept arbitrary data types

--- famine/src/numerical.rs
+++ famine/src/numerical.rs
@@ -1,7 +1,23 @@
+pub struct EulerSolver<T> {
+    pub state: Vec<T>,
+    pub derivatives: Vec<T>,
+    derivative_func: fn(&Vec<T>, &mut Vec<T>),
+}
 
-pub fn euler_step(dt: f32, state: &mut [f32], derivatives: &mut [f32], derivative_func: fn(&[f32], &[f32])) {
-    derivative_func(state, derivatives);
-    state.iter_mut()
-        .zip(derivatives)
-        .for_each(|(val, derivative)| { *val += *derivative * dt; });
+impl<T> EulerSolver<T> where
+    T: Copy + core::ops::Mul<f32> + std::ops::AddAssign<<T>::Output> {
+    pub fn new(initial_state: Vec<T>, derivative_func: fn(&Vec<T>, &mut Vec<T>)) -> Self {
+        EulerSolver {
+            state: initial_state.clone(),
+            derivatives: initial_state,
+            derivative_func
+        }
+    }
+
+    pub fn step(&mut self, dt: f32) {
+        (self.derivative_func)(&self.state, &mut self.derivatives);
+        self.state.iter_mut()
+            .zip(self.derivatives.iter())
+            .for_each(|(val, derivative)| { *val += *derivative * dt; });
+    }
 }
